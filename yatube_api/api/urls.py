5faeb760397131f7ab7525339e8c3da8ff424091
Имя Фамилия <alexandrmekhaev@yandex.ru>
2024-12-25 20:19:00 +0300

    readme

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework import generics
 
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
